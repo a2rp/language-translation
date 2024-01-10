d57595907aa5db1f45be99603631f69c6d259990
ash1198 <ash.ranjan09@gmail.com>
2024-01-11 02:28:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { t, i18n } = useTranslation();
+    const name = "Ashish Ranjan";
+    const handleClick = (language) => {
+        console.log(language);
+        i18n.changeLanguage(language);
+    };
+
+    return (
+        <div className="App">
+            <div style={{ display: "flex", gap: "15px", justifyContent: "center", marginTop: "15px" }}>
+                <input
+                    type="button"
+                    onClick={() => handleClick("en")}
+                    value="English"
+                />
+                <input
+                    type="button"
+                    onClick={() => handleClick("fr")}
+                    value="French"
+                />
+                <input
+                    type="button"
+                    onClick={() => handleClick("hin")}
+                    value="Hindi"
+                />
+            </div>
+            <h1>{t("greeting")}</h1>
+            <h1>{t("iAm", { name: name })}</h1>
+        </div>
+    );
 }
 
 export default App;
